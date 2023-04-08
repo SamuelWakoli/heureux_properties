270188011be7cd3d200de6ea48ce648ad76d2579
SamuelWakoli <swwakoli@gmail.com>
2023-04-08 13:58:16 +0300

    design for the details page

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
+import '../cards/card_functions.dart';
 
 class DetailsPage extends StatefulWidget {
   const DetailsPage({Key? key}) : super(key: key);
@@ -9,12 +12,90 @@ class DetailsPage extends StatefulWidget {
 
 /// This page uses [currentPropertyID] to load property details.
 class _DetailsPageState extends State<DetailsPage> {
+  /// Creates a Details Page card widget
+  Widget _displayImg(
+      {required context,
+      id,
+      required String propertyImg,
+      propertyName,
+      propertyPrice,
+      propertyLocation}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(18.0),
+              child: SizedBox(
+                  width: double.infinity,
+                  height: 200,
+                  child: Image.asset(
+                    propertyImg,
+                    fit: BoxFit.fitWidth,
+                  )),
+            ),
+          ),
+          SizedBox(height: 20),
+          const ListTile(
+            title: Text.rich(
+              TextSpan(
+                  text: "Property Name\n",
+                  style: TextStyle(fontSize: 20),
+                  children: [
+                    TextSpan(
+                      text:
+                          "For Sale\nPrice: Ksh. 1,000,000\nLocation: propertyLocation\n",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ]),
+            ),
+            subtitle: Text.rich(TextSpan(
+                text: "Property description text Property description text "
+                    "Property description text Property description text "
+                    "Property description text Property description text "
+                    "Property description text Property description text",
+                style: TextStyle(fontSize: 16))),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Call this function to return an image of a property.
+  /// Remember the 4 images, call this function 4 times in a list.
+  Widget _getImages(String image) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12.0),
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: image,
+          progressIndicatorBuilder: (context, url, downloadProgress) =>
+              CircularProgressIndicator(
+            value: downloadProgress.progress,
+            valueColor:
+                AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
+          ),
+          errorWidget: (context, url, error) => const Icon(Icons.error_outline),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Details"),
+      ),
+      body: ListView(
+        children: [
+          _displayImg(context: context, propertyImg: "assets/property1.jpg"),
+        ],
       ),
     );
   }
