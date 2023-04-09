6e48ada396169f5dc50af95d71f0ff47f8674526
SamuelWakoli <swwakoli@gmail.com>
2023-04-09 03:07:10 +0300

    loads payment history

--- lib/pages/payment_information.dart
+++ lib/pages/payment_information.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PaymentInfo extends StatefulWidget {
@@ -8,6 +10,8 @@ class PaymentInfo extends StatefulWidget {
 }
 
 class _PaymentInfoState extends State<PaymentInfo> {
+  String? userEmail = FirebaseAuth.instance.currentUser!.email.toString();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,90 +19,36 @@ class _PaymentInfoState extends State<PaymentInfo> {
         centerTitle: true,
         title: const Text("Payment History"),
       ),
-      body: ListView(
-        children: const [
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-          ListTile(
-            title: Text("PropertyName | Ksh. 20,000"),
-            subtitle: Text("Date: 21/04/2022"),
-          ),
-        ],
-      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .doc(userEmail)
+              .collection("payments")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor,
+                ),
+              );
+            }
+
+            if (snapshot.data!.docs.isEmpty) {
+              return const Center(
+                child: Text("No property payment made"),
+              );
+            }
+
+            return ListView(
+              children: snapshot.data!.docs
+                  .map((doc) => const ListTile(
+                        title: Text("PropertyName | Ksh. 20,000"),
+                        subtitle: Text("Date: 21/04/2022"),
+                      ))
+                  .toList(),
+            );
+          }),
     );
   }
 }
